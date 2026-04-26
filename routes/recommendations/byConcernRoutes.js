705a5cd95fd63f3fce577586dd57a98db7476bb4
adrianap0607 <132533905+adrianap0607@users.noreply.github.com>
2026-04-25 21:20:09 -0600

    feat: add skin concern endpoint (#4)

--- routes/recommendations/byConcernRoutes.js
+++ routes/recommendations/byConcernRoutes.js
@@ -1,3 +1,8 @@
 const router = require('express').Router();
+const {
+  getRecommendationsBySkinConcern
+} = require('../../controllers/recommendations/byConcernController');
+
+router.get('/:username', getRecommendationsBySkinConcern);
 
 module.exports = router;
